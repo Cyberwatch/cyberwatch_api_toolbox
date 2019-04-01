4beaba8d5242a3cae384a7867cb81533de95df02
Robin BECHON <robin@cyberwatch.fr>
2019-04-01 11:24:44 +0200

    Add sucess tests for method server/servers/get_detailed_servers
    Signed-off-by: Robin BECHON <robin@cyberwatch.fr>

--- spec/test_cbw_api.py
+++ spec/test_cbw_api.py
@@ -1,6 +1,8 @@
 """Test file for cbw_api.py"""
 
 from cbw_api_toolbox.cbw_api import CBWApi
+from cbw_api_toolbox.cbw_objects.cbw_server import CBWServer
+
 import vcr  # pylint: disable=import-error
 import pytest  # pylint: disable=import-error
 
@@ -29,3 +31,32 @@ class TestCBWApi:
         with pytest.raises(SystemExit) as exc:
             CBWApi('', API_KEY, SECRET_KEY).ping()
         assert exc.value.code == -1
+
+    def test_servers(self): # pylint: disable=no-self-use
+        """Tests for servers method"""
+        with vcr.use_cassette('spec/fixtures/vcr_cassettes/servers_ok.yaml'):
+            response = CBWApi(API_URL, API_KEY, SECRET_KEY).servers()
+            assert isinstance(response, list) is True
+            for server in response:
+                assert isinstance(server, CBWServer) is True
+
+    def test_server(self): # pylint: disable=no-self-use
+        """Tests for server method"""
+        with vcr.use_cassette('spec/fixtures/vcr_cassettes/server_ok.yaml'):
+            response = CBWApi(
+                API_URL,
+                API_KEY,
+                SECRET_KEY).server('25b4c2428fde60c311fb095e2083b33f')
+            assert isinstance(response, CBWServer) is True
+
+        with vcr.use_cassette('spec/fixtures/vcr_cassettes/server_failed.yaml'):
+            response = CBWApi(API_URL, API_KEY, SECRET_KEY).server('wrong_id')
+            assert isinstance(response, CBWServer) is False
+
+    def test_get_detailed_servers(self): # pylint: disable=no-self-use
+        """Tests for get_detailed_servers method"""
+        with vcr.use_cassette('spec/fixtures/vcr_cassettes/get_detailed_servers.yaml'):
+            response = CBWApi(API_URL, API_KEY, SECRET_KEY).get_detailed_servers()
+            assert isinstance(response, list) is True
+            for server in response:
+                assert isinstance(server, CBWServer) is True
