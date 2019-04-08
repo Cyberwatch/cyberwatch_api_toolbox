dbdcdd937e6ecfcbe65d9e15c10fef3e246d0c73
el harag <elharag@cyberwatch.fr>
2019-04-08 16:50:28 +0200

    Add method delete_server
    Signed-off-by: el harag <elharag@cyberwatch.fr>

---
+++ examples/delete_server.py
@@ -0,0 +1,18 @@
+from cbw_api_toolbox.cbw_api import CBWApi
+
+API_KEY = ''
+SECRET_KEY = ''
+API_URL = ''
+
+CLIENT = CBWApi(API_URL, API_KEY, SECRET_KEY)
+
+CLIENT.ping()
+
+SERVER_ID = '' #Id of the server you which to delete 
+
+result = CLIENT.delete_server(SERVER_ID) 
+
+if result:
+    print('Successfull deletion')
+else:
+    print('Failure')
