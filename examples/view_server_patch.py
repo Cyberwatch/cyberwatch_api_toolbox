4234c26fdf36596687f49e620ccabf5625fe7777
el harag <elharag@cyberwatch.fr>
2019-04-24 12:05:00 +0200

    view the server updates
    Signed-off-by: el harag <elharag@cyberwatch.fr>

--- examples/view_server_patch.py
+++ examples/view_server_patch.py
@@ -1,4 +1,4 @@
-"""view the server patch"""
+"""View the server updates"""
 
 from cbw_api_toolbox.cbw_api import CBWApi
 
@@ -8,7 +8,7 @@ API_URL = ""
 
 CLIENT = CBWApi(API_URL, API_KEY, SECRET_KEY)
 
-SERVER_ID = "" # add the server id
+SERVER_ID = ""  # add the server id
 
 server = CLIENT.server(SERVER_ID)
 
@@ -17,13 +17,12 @@ print("Update count : {}".format(server.updates_count))
 
 print("Updates :")
 for update in server.updates:
-    print("\t-Product : {0}\n"
-          "\t\t- Corrective action : {1} -> {2}".format(update["current"]["product"],
-                                                        update["current"]["version"],
+    print("\t-Product : {}".format(update["current"]["product"]))
+    print("\t\t- Corrective action : {0} -> {1}".format(update["current"]["version"],
                                                         update["target"]["version"]))
 
-    cve_list = ""
+    cve_list = []
     for cve in update["cve_announcements"]:
-        cve_list = cve_list + cve["cve_code"] + ", "
+        cve_list.append(cve["cve_code"])
 
-    print("\t\t- Cve List : {}".format(cve_list[:-2]))
+    print("\t\t- Cve List : {}".format(", ".join(cve_list)))
