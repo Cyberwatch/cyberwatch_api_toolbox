4f6fc66413a407dbebe136572a5e5ab7bebd03a1
Daniel Deutsch <daniel@cyberwatch.fr>
2019-10-03 17:17:57 +0200

    added description attribute to group model

--- cbw_api_toolbox/cbw_objects/cbw_group.py
+++ cbw_api_toolbox/cbw_objects/cbw_group.py
@@ -7,10 +7,12 @@ class CBWGroup:
     def __init__(self,
                  id="",  # pylint: disable=redefined-builtin
                  name="",
+                 description="",
                  created_at="",
                  updated_at="",
                  **kwargs): # pylint: disable=unused-argument
         self.id = id  # pylint: disable=invalid-name
         self.name = name
+        self.description = description
         self.created_at = created_at
         self.updated_at = updated_at
