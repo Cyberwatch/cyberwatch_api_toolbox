1aa9a267c2b91057f7fe6772999196fcffb4ee38
Daniel Deutsch <daniel@cyberwatch.fr>
2019-10-11 14:44:14 +0200

    example script to affect groups by OS

---
+++ examples/affect_group_os.py
@@ -0,0 +1,37 @@
+"""Script affecting a group depending on OS"""
+
+import os
+from configparser import ConfigParser
+from cbw_api_toolbox.cbw_api import CBWApi
+
+CONF = ConfigParser()
+CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
+CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get(
+    'cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
+
+LINUX_OS = ['Amazon', 'ArchLinux', 'Centos', 'Debian', 'Manjaro', 'Oracle', 'Ubuntu', 'Redhat', 'Suse']
+WINDOWS_OS = ['Windows']
+MAC_OS = ['Macos']
+
+
+def build_groups_list(server_id, system_os):
+    """Create list with system_os + other groups of server"""
+    server = CLIENT.server(server_id)
+    server_groups = [system_os]
+    if server.groups:
+        for group in server.groups:
+            server_groups.append(group.name)
+    return server_groups
+
+for server_item in CLIENT.servers():
+    if server_item.os is not None:
+        if server_item.os["type"][4:] in LINUX_OS:
+            OS = "LINUX"
+        elif server_item.os["type"][4:] in WINDOWS_OS:
+            OS = "WIN"
+        elif server_item.os["type"][4:] in MAC_OS:
+            OS = "MAC_OS"
+        else:
+            OS = "Other"
+    groups_list = build_groups_list(server_item.id, OS)
+    CLIENT.update_server(server_item.id, {'groups': ",".join(groups_list)})
