32914c9bec8971cc5d02863171f51662995298f9
Sulidi Maimaitiming <sulidi@cyberwatch.fr>
2020-04-22 17:59:52 +0200

    Implement Agent calls

---
+++ examples/agent_delete.py
@@ -0,0 +1,13 @@
+"""Delete agent"""
+
+import os
+from configparser import ConfigParser
+from cbw_api_toolbox.cbw_api import CBWApi
+
+CONF = ConfigParser()
+CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
+CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
+
+AGENT_ID = ''
+
+CLIENT.delete_agent(AGENT_ID)
