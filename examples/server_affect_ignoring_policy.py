e4c6ef431dc754fc2ad3c0fd402e0fda971d5ff5
Daniel Deutsch <daniel@cyberwatch.fr>
2020-07-07 10:59:59 +0200

    Affect Ignoring Policy script

---
+++ examples/server_affect_ignoring_policy.py
@@ -0,0 +1,19 @@
+"""Add an ignoring policy to all servers"""
+
+import os
+from configparser import ConfigParser
+from cbw_api_toolbox.cbw_api import CBWApi
+
+CONF = ConfigParser()
+CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
+CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
+
+# Find the ID on [CYBERWATCH_URL]/vm/ignoring_policies and editing the concerned policy (the ID will be in the URL)
+# Example: "https://[CYBERWATCH_URL]/vm/ignoring_policies/6/edit" will be "ignoring_policy_id": "6"
+
+INFO = {
+    "ignoring_policy_id": ""
+}
+
+for server_item in CLIENT.servers():
+    CLIENT.update_server(str(server_item.id), INFO)
