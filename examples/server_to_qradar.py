7b10482b5eb965390224cbe53510a23bf4a81547
Daniel Deutsch <daniel@cyberwatch.fr>
2020-07-08 10:47:38 +0200

    Improved example for QRadar integration with HTTP Receiver

--- examples/server_to_qradar.py
+++ examples/server_to_qradar.py
@@ -1,45 +1,85 @@
 """
-Example of sending data to qradar.
-Please provide a qradar url in your api.conf file using this format: 
+Example script for sending data to Qradar.
+Please provide a qradar url in your api.conf file using this format:
 
 [qradar]
 url = http://[QRADAR_URL]:[HTTP_RECEIVER_PORT]
 """
 
+import argparse
+from argparse import RawTextHelpFormatter
 import os
 import json
 from configparser import ConfigParser
 import requests
 from cbw_api_toolbox.cbw_api import CBWApi
 
-def post_qradar(url, data_json):
-    """
-        Send a post request to qradar HTTP Receiver
-    """
 
+def connect_api():
+    '''Connect to the API and test connection'''
+    conf = ConfigParser()
+    conf.read(os.path.join(os.path.abspath(
+        os.path.dirname(__file__)), '..', 'api.conf'))
+    client = CBWApi(conf.get('cyberwatch', 'url'), conf.get(
+        'cyberwatch', 'api_key'), conf.get('cyberwatch', 'secret_key'))
+
+    client.ping()
+    return client
+
+
+def get_url():
+    '''Get Qradar url from api.conf'''
+    conf = ConfigParser()
+    conf.read(os.path.join(os.path.abspath(
+        os.path.dirname(__file__)), '..', 'api.conf'))
+    client = conf.get('qradar', 'url')
+
+    return client
+
+
+def post_qradar(server, code_list):
+    '''Send a post request to Qradar HTTP Receiver'''
     headers = {'Content-Type': 'application/json'}
-    res = requests.post(url=url, headers=headers, data=data_json, verify=False)
+
+    payload = {
+        "EventCategory": "Cyberwatch Integration",
+        "Hostname": server.hostname,
+        "Status": server.status,
+        "Vulnerabilities": code_list
+    }
+
+    res = requests.post(url=get_url(), headers=headers,
+                        data=json.dumps(payload), verify=False)
     res.raise_for_status()
     return res.status_code
 
-# Load configuration from file api.conf
-CONF = ConfigParser()
-CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
-CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
 
-# Retrieve the computer from Cyberwatch API
-SERVER_ID = ''
-SERVER = CLIENT.server(SERVER_ID)
+def cve_code_list(server_id, client):
+    '''Build CVE Code list'''
+    server = client.server(str(server_id))
+    return list(map(lambda x: x.cve_code, server.cve_announcements))
 
-# Example payload to send to qradar
 
-payload = {
-    "EventCategory": "Cyberwatch Integration",
-    "Hostname": SERVER.hostname,
-    "Vulnerabilities count": SERVER.cve_announcements_count,
-}
+def launch_script():
+    '''Launch script'''
+    client = connect_api()
+    servers = client.servers()
+    for server in servers:
+        post_qradar(server, cve_code_list(server.id, client))
 
-payload_json = json.dumps(payload)
 
-# Send the data to qradar
-post_qradar(CONF.get('qradar', 'url'), payload)
+def main(args=None):
+    '''Main function'''
+
+    parser = argparse.ArgumentParser(
+        description="""Script using Cyberwatch API to send a Post request for each servers to Qradar HTTP Receiver.
+Please provide a qradar url in your api.conf file using this format:
+    [qradar]
+    url = http://[QRADAR_URL]:[HTTP_RECEIVER_PORT]""", formatter_class=RawTextHelpFormatter)
+
+    parser.parse_args(args)
+    launch_script()
+
+
+if __name__ == '__main__':
+    main()
