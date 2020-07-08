aacd7235c83be285f99af952324cfcfa0948bf44
Daniel Deutsch <daniel@cyberwatch.fr>
2020-07-08 09:31:45 +0200

    Fix ignoring policy script

--- examples/server_affect_ignoring_policy.py
+++ examples/server_affect_ignoring_policy.py
@@ -8,11 +8,11 @@ CONF = ConfigParser()
 CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
 CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
 
-# Find the ID on [CYBERWATCH_URL]/vm/ignoring_policies and editing the concerned policy (the ID will be in the URL)
-# Example: "https://[CYBERWATCH_URL]/vm/ignoring_policies/6/edit" will be "ignoring_policy_id": "6"
+# Find the ID on [CYBERWATCH_URL]/vm/ignoring_policies and edit the concerned policy (the ID will be in the URL)
+# Example: "https://[CYBERWATCH_URL]/vm/ignoring_policies/6/edit" will be "ignoring_policy": "6"
 
 INFO = {
-    "ignoring_policy_id": ""
+    "ignoring_policy": ""
 }
 
 for server_item in CLIENT.servers():
