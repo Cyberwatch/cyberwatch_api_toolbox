55401923b195a6f46bea44d9fb87262795092d9b
Daniel D <daniel@cyberwatch.fr>
2020-09-22 13:52:48 +0200

    Refactored + clean servers_details_export_xlsx.py

--- examples/servers_detail_export_xlsx.py
+++ examples/servers_detail_export_xlsx.py
@@ -1,168 +1,172 @@
-"""Example: export vulnerabilities to XLSX"""
+"""Script using Cyberwatch API to export vulnerabilities of servers with details to XLSX"""
 
 import os
 from configparser import ConfigParser
-import xlsxwriter  # pylint: disable=import-error
+import xlsxwriter
 from cbw_api_toolbox.cbw_api import CBWApi
 
-CONF = ConfigParser()
-CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
-CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
+def connect_api():
+    '''Connect to the API and test connection'''
+    conf = ConfigParser()
+    conf.read(os.path.join(os.path.abspath(
+        os.path.dirname(__file__)), '..', 'api.conf'))
+    client = CBWApi(conf.get('cyberwatch', 'url'), conf.get(
+        'cyberwatch', 'api_key'), conf.get('cyberwatch', 'secret_key'))
 
-CLIENT.ping()
+    client.ping()
+    return client
 
-SERVERS = CLIENT.servers()
+def servers_details(client):
+    """Build a list with each server and it's details"""
+    servers_list = []
+    for server in client.servers():
+        server = client.server(str(server.id))
+        servers_list.append(server)
+    return servers_list
 
-EXPORTED = xlsxwriter.Workbook('export.xlsx')
+def export_xls(client):
+    """Export differents categories to the XLSX file"""
+    file = xlsxwriter.Workbook('export.xlsx')
+    servers = servers_details(client)
+    # Create each category tab
+    computer_tab = file.add_worksheet("Computers")
+    vulnerabilities_tab = file.add_worksheet("Vulnerabilities")
+    recommended_tab = file.add_worksheet("Recommended Actions")
 
-# Create each category tab
-COMPUTER = EXPORTED.add_worksheet("Computers")
-VULNERABILITIES = EXPORTED.add_worksheet("Vulnerabilities")
-SECURITY = EXPORTED.add_worksheet("Security Advisories")
-RECOMMENDED = EXPORTED.add_worksheet("Recommended Actions")
+    host_details(computer_tab, servers)
+    vulnerabilities_details(vulnerabilities_tab, servers, client)
+    recommended_actions_details(recommended_tab, servers)
 
-# Build a list with each server and it's details
-SERVERS_LIST = []
-for server in SERVERS:
-    server = CLIENT.server(str(server.id))
-    SERVERS_LIST.append(server)
+    file.close()
 
-ROW = 0
-COL = 0
-# Write each Host and it's details in `Computers` tab
-for server in SERVERS_LIST:
-    COMPUTER.write(ROW, COL, "Hostname")
-    COMPUTER.write(ROW + 1, COL, server.hostname)
+def host_details(computer_tab, servers):
+    """Write each Host and it's details in `Computers` tab"""
+    row = 0
+    col = 0
+    for server in servers:
+        computer_tab.write(row, col, "Hostname")
+        computer_tab.write(row + 1, col, server.hostname)
 
-    COMPUTER.write(ROW, COL + 1, "OS")
-    COMPUTER.write(ROW + 1, COL + 1, server.os.name)
+        computer_tab.write(row, col + 1, "OS")
+        computer_tab.write(row + 1, col + 1, server.os.name)
 
-    COMPUTER.write(ROW, COL + 2, "Groups")
-    if server.groups:
-        GROUPE_NAME = ""
-        for group in server.groups:
-            GROUPE_NAME += group.name + ", "
-        GROUPE_NAME = GROUPE_NAME[:-1]
-        COMPUTER.write(ROW + 1, COL + 2, GROUPE_NAME)
+        computer_tab.write(row, col + 2, "Groups")
+        if server.groups:
+            group_name = ""
+            for group in server.groups:
+                group_name += group.name + ", "
+            group_name = group_name[:-1]
+            computer_tab.write(row + 1, col + 2, group_name)
 
-    COMPUTER.write(ROW, COL + 3, "Status")
-    COMPUTER.write(ROW + 1, COL + 3, server.status)
+        computer_tab.write(row, col + 3, "Status")
+        computer_tab.write(row + 1, col + 3, server.status)
 
-    COMPUTER.write(ROW, COL + 4, "Environment")
-    COMPUTER.write(ROW + 1, COL + 4, server.environment.name)
+        computer_tab.write(row, col + 4, "Environment")
+        computer_tab.write(row + 1, col + 4, server.environment.name)
 
-    COMPUTER.write(ROW, COL + 5, "Category")
-    COMPUTER.write(ROW + 1, COL + 5, server.category)
+        computer_tab.write(row, col + 5, "Category")
+        computer_tab.write(row + 1, col + 5, server.category)
 
-    ROW += 3
+        row += 3
 
-ROW = 0
-COL = 0
-# Write vulnerabilities of each Host and it's details in `Vulnerabilities` tab
-for server in SERVERS_LIST:
-    VULNERABILITIES.write(ROW, COL, server.hostname)
-    ROW += 1
-    VULNERABILITIES.write(ROW, COL, "Vulnerabilty")
-    VULNERABILITIES.write(ROW, COL + 1, "CVSS score v3")
-    VULNERABILITIES.write(ROW, COL + 2, "CVSS score v2")
-    VULNERABILITIES.write(ROW, COL + 3, "Exploitable")
-    VULNERABILITIES.write(ROW, COL + 4, "Description")
-    VULNERABILITIES.write(ROW, COL + 5, "Access vector v2")
-    VULNERABILITIES.write(ROW, COL + 6, "Access complexity v2")
-    VULNERABILITIES.write(ROW, COL + 7, "Authentication requirement v2")
-    VULNERABILITIES.write(ROW, COL + 8, "Confidentiality impact v2")
-    VULNERABILITIES.write(ROW, COL + 9, "Integrity impact v2")
-    VULNERABILITIES.write(ROW, COL + 10, "Availability impact v2")
-    VULNERABILITIES.write(ROW, COL + 11, "Access vector v3")
-    VULNERABILITIES.write(ROW, COL + 12, "Access complexity v3")
-    VULNERABILITIES.write(ROW, COL + 13, "Privilege requirement v3")
-    VULNERABILITIES.write(ROW, COL + 14, "User Authentication v3")
-    VULNERABILITIES.write(ROW, COL + 15, "Integrity impact v3")
-    VULNERABILITIES.write(ROW, COL + 16, "Availability impact v3")
-    VULNERABILITIES.write(ROW, COL + 17, "Confidentiality impact v3")
-    VULNERABILITIES.write(ROW, COL + 18, "Scope v3")
+def vulnerabilities_details(vulnerabilities_tab, servers, client):
+    """Write vulnerabilities of each Host and it's details in `Vulnerabilities` tab"""
+    row = 0
+    col = 0
+    for server in servers:
+        vulnerabilities_tab.write(row, col, server.hostname)
+        row += 1
+        vulnerabilities_tab.write(row, col, "Vulnerabilty")
+        vulnerabilities_tab.write(row, col + 1, "CVSS score v3")
+        vulnerabilities_tab.write(row, col + 2, "CVSS score v2")
+        vulnerabilities_tab.write(row, col + 3, "Exploitable")
+        vulnerabilities_tab.write(row, col + 4, "Description")
+        vulnerabilities_tab.write(row, col + 5, "Access vector v2")
+        vulnerabilities_tab.write(row, col + 6, "Access complexity v2")
+        vulnerabilities_tab.write(row, col + 7, "Authentication requirement v2")
+        vulnerabilities_tab.write(row, col + 8, "Confidentiality impact v2")
+        vulnerabilities_tab.write(row, col + 9, "Integrity impact v2")
+        vulnerabilities_tab.write(row, col + 10, "Availability impact v2")
+        vulnerabilities_tab.write(row, col + 11, "Access vector v3")
+        vulnerabilities_tab.write(row, col + 12, "Access complexity v3")
+        vulnerabilities_tab.write(row, col + 13, "Privilege requirement v3")
+        vulnerabilities_tab.write(row, col + 14, "User Authentication v3")
+        vulnerabilities_tab.write(row, col + 15, "Integrity impact v3")
+        vulnerabilities_tab.write(row, col + 16, "Availability impact v3")
+        vulnerabilities_tab.write(row, col + 17, "Confidentiality impact v3")
+        vulnerabilities_tab.write(row, col + 18, "Scope v3")
 
-    # Get details of each CVE
-    for cve in server.cve_announcements:
-        cve = CLIENT.cve_announcement(cve.cve_code)
+        # Get details of each CVE
+        for cve in server.cve_announcements:
+            cve = client.cve_announcement(cve.cve_code)
 
-        VULNERABILITIES.write(ROW + 1, COL, cve.cve_code)
-        VULNERABILITIES.write(ROW + 1, COL + 1, cve.score_v3)
-        VULNERABILITIES.write(ROW + 1, COL + 2, cve.score_v2)
-        VULNERABILITIES.write(ROW + 1, COL + 3, cve.exploit_code_maturity)
-        VULNERABILITIES.write(ROW + 1, COL + 4, cve.content)
-        if cve.cvss is not None:
-            VULNERABILITIES.write(ROW + 1, COL + 5, cve.cvss.access_vector)
-            VULNERABILITIES.write(ROW + 1, COL + 6, cve.cvss.access_complexity)
-            VULNERABILITIES.write(ROW + 1, COL + 7, cve.cvss.authentication)
-            VULNERABILITIES.write(ROW + 1, COL + 8, cve.cvss.availability_impact)
-            VULNERABILITIES.write(ROW + 1, COL + 9, cve.cvss.confidentiality_impact)
-            VULNERABILITIES.write(ROW + 1, COL + 10, cve.cvss.integrity_impact)
+            vulnerabilities_tab.write(row + 1, col, cve.cve_code)
+            vulnerabilities_tab.write(row + 1, col + 1, cve.score_v3)
+            vulnerabilities_tab.write(row + 1, col + 2, cve.score_v2)
+            vulnerabilities_tab.write(row + 1, col + 3, cve.exploit_code_maturity)
+            vulnerabilities_tab.write(row + 1, col + 4, cve.content)
 
-        if cve.cvss_v3 is not None:
-            VULNERABILITIES.write(ROW + 1, COL + 11, cve.cvss_v3.access_vector)
-            VULNERABILITIES.write(ROW + 1, COL + 12, cve.cvss_v3.access_complexity)
-            VULNERABILITIES.write(ROW + 1, COL + 13, cve.cvss_v3.privileges_required)
-            VULNERABILITIES.write(ROW + 1, COL + 14, cve.cvss_v3.user_interaction)
-            VULNERABILITIES.write(ROW + 1, COL + 15, cve.cvss_v3.integrity_impact)
-            VULNERABILITIES.write(ROW + 1, COL + 16, cve.cvss_v3.availability_impact)
-            VULNERABILITIES.write(ROW + 1, COL + 17, cve.cvss_v3.confidentiality_impact)
-            VULNERABILITIES.write(ROW + 1, COL + 18, cve.cvss_v3.scope)
+            if cve.cvss is not None:
+                vulnerabilities_tab.write(row + 1, col + 5, cve.cvss.access_vector)
+                vulnerabilities_tab.write(row + 1, col + 6, cve.cvss.access_complexity)
+                vulnerabilities_tab.write(row + 1, col + 7, cve.cvss.authentication)
+                vulnerabilities_tab.write(row + 1, col + 8, cve.cvss.availability_impact)
+                vulnerabilities_tab.write(row + 1, col + 9, cve.cvss.confidentiality_impact)
+                vulnerabilities_tab.write(row + 1, col + 10, cve.cvss.integrity_impact)
 
-        ROW += 1
-    ROW += 2
+            if cve.cvss_v3 is not None:
+                vulnerabilities_tab.write(row + 1, col + 11, cve.cvss_v3.access_vector)
+                vulnerabilities_tab.write(row + 1, col + 12, cve.cvss_v3.access_complexity)
+                vulnerabilities_tab.write(row + 1, col + 13, cve.cvss_v3.privileges_required)
+                vulnerabilities_tab.write(row + 1, col + 14, cve.cvss_v3.user_interaction)
+                vulnerabilities_tab.write(row + 1, col + 15, cve.cvss_v3.integrity_impact)
+                vulnerabilities_tab.write(row + 1, col + 16, cve.cvss_v3.availability_impact)
+                vulnerabilities_tab.write(row + 1, col + 17, cve.cvss_v3.confidentiality_impact)
+                vulnerabilities_tab.write(row + 1, col + 18, cve.cvss_v3.scope)
 
-ROW = 0
-COL = 0
-# Write each Host security announcements with details in `Security Advisories` tab
-for server in SERVERS_LIST:
-    SECURITY.write(ROW, COL, server.hostname)
-    ROW += 1
+            row += 1
+        row += 2
 
-    SECURITY.write(ROW, COL, "Security announcement code")
-    SECURITY.write(ROW, COL + 1, "CVE Announcement")
-    SECURITY.write(ROW, COL + 2, "Link")
-    SECURITY.write(ROW, COL + 3, "Published date")
-    SECURITY.write(ROW, COL + 4, "Updated date")
+def recommended_actions_details(recommended_tab, servers):
+    """Write each recommended actions/affected technologies of each Host in `Recommended Actions` tab"""
+    row = 0
+    col = 0
+    for server in servers:
+        recommended_tab.write(row, col, server.hostname)
+        row += 1
 
-    for security_announcement in server.security_announcements:
-        SECURITY.write(ROW + 1, COL, security_announcement.sa_code)
-        SECURITY.write(ROW + 1, COL + 2, security_announcement.link)
-        SECURITY.write(ROW + 1, COL + 3, security_announcement.created_at)
-        SECURITY.write(ROW + 1, COL + 4, security_announcement.updated_at)
+        recommended_tab.write(row, col, "Affected technology")
+        recommended_tab.write(row, col + 1, "CVE announcement")
+        recommended_tab.write(row, col + 2, "Patchable")
+        recommended_tab.write(row, col + 3, "Current version")
+        recommended_tab.write(row, col + 4, "Target version")
 
-        for cve in security_announcement.cve_announcements:
-            CVE_CODE_LIST += cve.cve_code + ", "
-        CVE_CODE_LIST = CVE_CODE_LIST[:-1]
-        SECURITY.write(ROW + 1, COL + 1, CVE_CODE_LIST)
+        for update in server.updates:
+            if update.current is not None:
+                recommended_tab.write(row + 1, col, update.current.product)
+                recommended_tab.write(row + 1, col + 3, update.current.version)
+                recommended_tab.write(row + 1, col + 1, ', '.join(update.cve_announcements))
+                recommended_tab.write(row + 1, col + 2, update.patchable)
+                recommended_tab.write(row + 1, col + 4, update.target.version)
+            else:
+                recommended_tab.write(row + 1, col, update.target.product)
+                recommended_tab.write(row + 1, col + 1, ', '.join(update.cve_announcements))
+                recommended_tab.write(row + 1, col + 2, update.patchable)
 
-        ROW += 1
-    ROW += 2
+            row += 1
+        row += 2
 
-ROW = 0
-COL = 0
-# Write each recommended actions/affected technologies of each Host in `Recommended Actions` tab
-for server in SERVERS_LIST:
-    RECOMMENDED.write(ROW, COL, server.hostname)
-    ROW += 1
 
-    RECOMMENDED.write(ROW, COL, "Affected technology")
-    RECOMMENDED.write(ROW, COL + 1, "CVE announcement")
-    RECOMMENDED.write(ROW, COL + 2, "Patchable")
-    RECOMMENDED.write(ROW, COL + 3, "Current version")
-    RECOMMENDED.write(ROW, COL + 4, "Target version")
+def launch_script():
+    '''Launch script'''
+    client = connect_api()
+    export_xls(client)
+    print("INFO: Done.")
 
-    for update in server.updates:
-        for cve in update.cve_announcements:
-            CVE_CODE_LIST += cve.cve_code + ", "
-        CVE_CODE_LIST = CVE_CODE_LIST[:-1]
-        RECOMMENDED.write(ROW + 1, COL, update.current.product)
-        RECOMMENDED.write(ROW + 1, COL + 1, CVE_CODE_LIST)
-        RECOMMENDED.write(ROW + 1, COL + 2, update.patchable)
-        RECOMMENDED.write(ROW + 1, COL + 3, update.current.version)
-        RECOMMENDED.write(ROW + 1, COL + 4, update.target.version)
+def main():
+    '''Main function'''
+    launch_script()
 
-        ROW += 1
-    ROW += 2
 
-EXPORTED.close()
+if __name__ == '__main__':
+    main()
