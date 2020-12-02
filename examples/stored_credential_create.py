966639b27b6dad46fe58afc32b448f9428eee8a0
PierreHachard <pierre@cyberwatch.fr>
2020-12-02 12:02:15 +0100

    add example for redmine integration

--- examples/stored_credential_create.py
+++ examples/stored_credential_create.py
@@ -8,17 +8,17 @@ CONF = ConfigParser()
 CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
 CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
 
-INFO = {"type": "", #mandatory, precises the type of the stored credentials
-        "name": "", #mandatory, precises the name of the stored credentials
-        "endpoint": "", #entry point (URL) for credentials of type docker engine or docker registry.
-        "user": "", #precises the user of the stored credentials
-        "password": "", #precises the password in the stored credentials
-        "key": "", #precises the secret key for credentials of type key
-        "ca_cert": "", #certificate of the certificate authority for TLS credentials like a docker engine.
-        "client_cert": "", #precise client private key for TLS credentials like a Docker engine.
-        "client_key": "", #client certificate for TLS credentials like a Docker engine.
-        "auth_password": "", #for SNMP, authentication password for SNMP credentials.
-        "priv_password": "" #for SNMP, encryption password for SNMP credentials.
-        }
+INFO = {"type": "", # mandatory, specifies the type of the stored credentials
+        "name": "", # mandatory, specifies the name of the stored credentials
+        "user": "", # specifies the user of the stored credentials
+        "password": "", # specifies the password in the stored credentials
+        "key": "", # specifies the secret key for credentials of type key
+        "endpoint": "", # entry point (URL) for credentials of type docker engine or docker registry
+        "ca_cert": "", # certificate of the certificate authority for TLS credentials like a docker engine
+        "client_cert": "", # specifies client private key for TLS credentials like a Docker engine
+        "client_key": "", # client certificate for TLS credentials like a Docker engine
+        "auth_password": "", # for SNMP, authentication password for SNMP credentials
+        "priv_password": "" # for SNMP, encryption password for SNMP credentials
+}
 
 CLIENT.create_stored_credential(INFO)
