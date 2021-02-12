c46c2d310d6ae2633b08308c941df6947d7598e0
Daniel Deutsch <daniel@cyberwatch.fr>
2021-02-12 11:49:55 +0100

    Added routes for docker images + unit tests

---
+++ examples/docker_image_delete.py
@@ -0,0 +1,13 @@
+"""Delete docker image"""
+
+import os
+from configparser import ConfigParser
+from cbw_api_toolbox.cbw_api import CBWApi
+
+CONF = ConfigParser()
+CONF.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'api.conf'))
+CLIENT = CBWApi(CONF.get('cyberwatch', 'url'), CONF.get('cyberwatch', 'api_key'), CONF.get('cyberwatch', 'secret_key'))
+
+DOCKER_IMAGE_ID = '' # ID of the docker image.
+
+CLIENT.delete_docker_image(DOCKER_IMAGE_ID)
