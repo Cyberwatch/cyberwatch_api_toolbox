19bce4de4e33bd454b963c78dc482a9de05b65c5
polyedre <polyedre@disroot.org>
2021-03-10 11:31:27 +0100

    cli(docker-show-vuln): Add --format argument with xml output

--- cli/docker_image/show.py
+++ cli/docker_image/show.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import xml.etree.ElementTree as ET
 
 
 def configure_parser(docker_image_subparser):
@@ -37,6 +38,11 @@ def init_show_vulnerabilities_subcommand(docker_image_show_subparser):
         )
     )
     docker_image_show_vulnerabilities_parser.add_argument("docker_image_id")
+    docker_image_show_vulnerabilities_parser.add_argument(
+        "--format",
+        default="text",
+        help="Format supported are 'junit-xml' and 'text'.",
+    )
 
 
 def vulnerabilities_subcommand(args, api):
@@ -53,16 +59,38 @@ def vulnerabilities_subcommand(args, api):
     server_id = str(docker_image[6])
     server = api.server(server_id)
 
+    cve_announcements = []
     vulnerabilities = server.cve_announcements
+    for vulnerability in vulnerabilities:
+        cve_code = vulnerability.cve_code
+        cve_announcements.append(api.cve_announcement(cve_code))
+    generate_output(cve_announcements, docker_image, args, api)
+
+    if vulnerabilities:
+        exit(1)
+
+
+def generate_output(cve_announcements, docker_image, args, api):
+    if args.format == "text":
+        generate_text_output(cve_announcements)
+    elif args.format == "junit-xml":
+        generate_junit_xml_output(cve_announcements, docker_image, api)
+    else:
+        print(
+            f"The following format is not supported: '{args.format}'.",
+            file=sys.stderr,
+        )
+        exit(1)
+
+
+def generate_text_output(cve_announcements):
     CVE_ANNOUNCEMENT_FORMAT_STRING = "{cve_code:14} {score:<5} {technologies}"
     print(
         CVE_ANNOUNCEMENT_FORMAT_STRING.format(
             cve_code="CVE", score="SCORE", technologies="TECHNOLOGIES"
         )
     )
-    for vulnerability in vulnerabilities:
-        cve_code = vulnerability.cve_code
-        cve_announcement = api.cve_announcement(cve_code)
+    for cve_announcement in cve_announcements:
         print(
             CVE_ANNOUNCEMENT_FORMAT_STRING.format(
                 cve_code=cve_announcement.cve_code,
@@ -74,6 +102,39 @@ def vulnerabilities_subcommand(args, api):
             )
         )
 
-    if vulnerabilities:
-        print("Some vulnerabilities have been found")
-        exit(1)
+
+def generate_junit_xml_output(cve_announcements, docker_image, api):
+    testsuite = ET.Element(
+        "testsuite", name="Vulnerabilities for docker image"
+    )
+    for cve_announcement in cve_announcements:
+        technos = ",".join(
+            f"{techno.vendor}:{techno.product}"
+            for techno in cve_announcement.technologies
+        )
+        testcase = ET.SubElement(
+            testsuite,
+            "testcase",
+            classname=f"{docker_image[1]}:{docker_image[2]}",
+            name=(
+                f"Vulnerability {cve_announcement.cve_code} "
+                f"has been detected on technologies {technos} "
+                f"with a score of {cve_announcement.score}"
+            ),
+            file=cve_announcement.cve_code,
+            time="0.0",
+        )
+        failure = ET.SubElement(
+            testcase, "failure", message=f"A vulnerability has been found."
+        )
+        failure.text = prettify_cve_announcement(cve_announcement, api.api_url)
+    print(ET.tostring(testsuite).decode())
+
+
+def prettify_cve_announcement(cve_announcement, api_url):
+    return (
+        f"[{cve_announcement.cve_code}] {cve_announcement.content}\n"
+        f"Score: {cve_announcement.score}\n"
+        f"Exploit code maturity: {cve_announcement.exploit_code_maturity}\n"
+        f"Link: {api_url}/cve_announcements/{cve_announcement.cve_code}\n"
+    )
